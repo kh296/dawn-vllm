add8511908f583a0dc7ff1c3aa950e04851da974
K. Harrison <kh296@users.noreply.github.com>
2026-04-14 09:51:31 +0100

    Added more example code

---
+++ examples/go_vllm_bench_serve.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -l
+#SBATCH --job-name=vllm_bench_serve # create a short name for the job
+#SBATCH --output=%x_%j.log      # job output file
+#SBATCH --partition=pvc9        # cluster partition to be used
+#SBATCH --nodes=1               # number of nodes
+#SBATCH --gres=gpu:4            # number of allocated gpus per node
+#SBATCH --time=01:00:00         # total run time limit (HH:MM:SS)
+
+# Script for starting a vLLM server on one or multiple nodes,
+# and from another node running a benchmark test or online serving througput.
+#
+# This script can be run interactively on a compute node:
+#     ./go_vllm_bench_serve.sh
+# or can be submitted to a Slurm batch system, substituting
+# valid project account for <project_account>:.
+#     sbatch --account=<project_account> ./go_vllm_bench_serve.sh
+#
+# Results from the benchmark test are written to:
+# "vllm_bench_serve_<id>_subjob.log"
+# Here, <id> will be "${SLURM_JOB_ID}" if this is non-null at the time
+# of submitting the benchmarking job, or otherwise will be
+# date and time in the format YYYY:mm:dd_HH:MM:SS.
+
+# Unset and set Slurm variables for compatibility with srun.
+unset SLURM_MEM_PER_CPU
+unset SLURM_MEM_PER_NODE
+SLURM_EXPORT_ENV="ALL"
+
+# Ensure PROJECT_HOME defined.
+if [[ ! -d "${PROJECT_HOME}" ]]; then
+    PROJECT_HOME=$(cd $(dirname "${BASH_SOURCE[0]}")/..; pwd)
+    if [[ ${PROJECT_HOME} == /var/spool/* ]]; then
+        PROJECT_HOME=$(dirname $(pwd))
+    fi
+fi
+
+# Generate an API key.
+API_KEY=$(pwgen 16 1)
+
+# Start a vLLM server.
+VLLM_API_KEY=${API_KEY} ./go_vllm.sh -a -r vllm_serve &
+
+# Wait until server startup has completed.
+while ! ss -tunlp | grep -q vllm; do
+  sleep 10
+done
+
+if [[ -z "${SLURM_NNODES}" ]]; then
+    SLURM_NNODES=1
+fi
+
+if [[ "${SLURM_NNODES}" -gt "1" ]]; then
+    # Wait here until the ray head node is detected.
+export HEAD_NODE_PORT=$(( (SLURM_JOB_ID % 10000) + 50000 ))
+export HEAD_NODE_ADDRESS="${HEAD_NODE_IP}:${HEAD_NODE_PORT}"
+    until ray status --address=${HEAD_NODE_ADDRESS} >/dev/null 2>&1; do
+        sleep 10
+    done
+fi
+
+# Submit a job to run the benchmark test.
+TIMESTAMP="$(date +"%Y:%m:%d_%H:%M:%S")"
+LOG_FILE="vllm_bench_serve_${SLURM_JOB_ID:-${TIMESTAMP}}_subjob.log"
+sbatch --wait --nodes=1 --gres=gpu:1 --export=OPENAI_API_KEY=${API_KEY},VLLM_HOST=$(hostname) --output="LOG_FILE" ./go_vllm.sh -a -r vllm_bench_serve
---
+++ examples/openai_chat.py
@@ -0,0 +1,77 @@
+"""
+Example code for interacting with model served by vLLM.
+
+For information about the OpenAI Python API library, see:
+    https://github.com/openai/openai-python
+"""
+from openai import OpenAI
+import os
+import re
+import socket
+
+def get_base_url(
+    default_vllm_host=socket.gethostname(), default_vllm_port="8000"):
+    """
+    Return URL that can be used with OpenAI client.
+
+    The host and client components are set from environment variables,
+    if defined, or otherwise are set to default values.
+    """
+    vllm_host = os.environ.get("VLLM_HOST", default_vllm_host)
+    vllm_port = os.environ.get("VLLM_PORT", default_vllm_port)
+    return f"http://{vllm_host}:{vllm_port}/v1"
+
+def no_think_block(text):
+    """
+    Return input text with any think block(s) removed.
+
+    A think block may be included, for example, in an OpenAI response.
+    """
+    return re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL).strip()
+
+def print_latest(history):
+    """
+    Return formatted output for last item in input history list.
+
+    Each history item is assumed to be a dictionary
+    with keys "role" and "content", such as used in OpenAI messages.
+    """
+    print(f"{history[-1]['role']}:\n{history[-1]['content']}\n")
+
+# Create OpenAI client.
+client = OpenAI(base_url=get_base_url())
+
+# Initialise message history.
+history = [{"role": "system", "content": "You are an expert coder."}]
+print_latest(history)
+
+# Obtain and record first OpenAI response.
+history.append({"role": "user", "content":
+    ("Suggest an exercise, and its solution, for "
+     "students who have completed an intermediate-level Python course.")})
+print_latest(history)
+
+response = client.responses.create(
+    model = "Qwen/Qwen3-4B",
+    input = history,
+    store = False,
+    )
+
+assistant_content = no_think_block(response.output_text)
+history.append({"role": "assistant", "content": assistant_content})
+print_latest(history)
+
+# Obtain and record second OpenAI response.
+history.append({"role": "user",
+    "content": "Suggest a second exercise and its solution."})
+print_latest(history)
+
+response = client.responses.create(
+    model = "Qwen/Qwen3-4B",
+    input = history,
+    store = False,
+    )
+
+assistant_content = no_think_block(response.output_text)
+history.append({"role": "assistant", "content": assistant_content})
+print_latest(history)
