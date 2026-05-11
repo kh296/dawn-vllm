5d2cad4811a4863a633d98c5c758e4c336c0f9ec
kh296 <kh296@users.noreply.github.com>
2026-05-11 17:12:10 +0100

    Check connection to server before submitting requests

--- examples/openai_chat.py
+++ examples/openai_chat.py
@@ -4,10 +4,45 @@ Example code for interacting with model served by vLLM.
 For information about the OpenAI Python API library, see:
     https://github.com/openai/openai-python
 """
-from openai import OpenAI
 import os
 import re
 import socket
+import sys
+import time
+
+from openai import APIConnectionError, OpenAI
+
+
+def can_connect(client, interval=60, max_wait=1200, verbose=True):
+    """
+    Check whether connection to client can be established.
+
+    With the specified interval between them, attempts to connect to the
+    client are repeated until either an attempt is successful (return True)
+    of the time elapsed since the first attempt exceeds max_wait (return False).
+    Progress information is printed if the verbose parameter is set to True.
+    """
+    wait_time = 0
+    while True:
+        try:
+            client.models.list()
+            if verbose:
+                print("Connected to vLLM server.\n", flush=True)
+            return True
+        except APIConnectionError:
+            if verbose:
+                print("Unable to connect to vLLM server:")
+            status = f"    wait time: {wait_time} s; max wait: {max_wait} s"
+            if wait_time >= max_wait:
+                if verbose:
+                    print(f"{status}; exiting")
+                    return False
+            else:
+                if verbose:
+                    print(f"{status}; retry in {interval} s", flush=True)
+                wait_time += interval
+                time.sleep(interval)
+                      
 
 def get_base_url(
     default_vllm_host=socket.gethostname(), default_vllm_port="8000"):
@@ -21,6 +56,7 @@ def get_base_url(
     vllm_port = os.environ.get("VLLM_PORT", default_vllm_port)
     return f"http://{vllm_host}:{vllm_port}/v1"
 
+
 def no_think_block(text):
     """
     Return input text with any think block(s) removed.
@@ -29,6 +65,7 @@ def no_think_block(text):
     """
     return re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL).strip()
 
+
 def print_latest(history):
     """
     Return formatted output for last item in input history list.
@@ -36,10 +73,13 @@ def print_latest(history):
     Each history item is assumed to be a dictionary
     with keys "role" and "content", such as used in OpenAI messages.
     """
-    print(f"{history[-1]['role']}:\n{history[-1]['content']}\n")
+    print(f"{history[-1]['role']}:\n{history[-1]['content']}\n", flush=True)
+
 
 # Create OpenAI client.
 client = OpenAI(base_url=get_base_url())
+if not can_connect(client):
+    sys.exit(1)
 
 # Initialise message history.
 history = [{"role": "system", "content": "You are an expert coder."}]
